871e5dab933d0cc20e7bb8bfd3c6417e6bb709c6
Jackie <jzhou39@gmail.com>
2023-03-14 00:06:52 -0400

    more progress, moving camera after delay

--- Project-Sample-For-Shared-Workspace/Assets/Scripts/MiniGamePlinko/CameraFollow.cs
+++ Project-Sample-For-Shared-Workspace/Assets/Scripts/MiniGamePlinko/CameraFollow.cs
@@ -2,34 +2,49 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject objectToFollow;
+    //public GameObject objectToFollow;
+
+    public Transform objectToFollow;
+
     private bool delayComplete = false;
+    public float followSpeed = 5f;
+    public float minYPosition = 0.25f;
+    public float delay = 2f;
+
+    private void Start()
+    {
+        delayComplete = false;
+    }
 
     private void Update()
     {
         if (!delayComplete)
         {
             // Wait for one second
-            Invoke("CompleteDelay", 1f);
+            Invoke("CompleteDelay", delay);
         }
-    }
-
-    private void LateUpdate()
-    {
-        if (delayComplete)
+        if (objectToFollow != null && delayComplete)
         {
-            // Calculate the desired position of the camera
-            Vector3 targetPosition = transform.position;
-            targetPosition.y = objectToFollow.transform.position.y;
+            // Calculate the desired Y position of the camera
+            float targetYPosition = objectToFollow.position.y;
 
-            // Smoothly move the camera to the target position over time
-            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+            // Limit the Y position of the camera
+            targetYPosition = Mathf.Max(targetYPosition, minYPosition);
+
+            // Calculate the current position of the camera
+            Vector3 currentPosition = transform.position;
+
+            // Set the X and Z positions of the current position to the target Y position
+            currentPosition.y = targetYPosition;
+
+            // Move the camera towards the current position over time
+            transform.position = Vector3.MoveTowards(transform.position, currentPosition, followSpeed * Time.deltaTime);
         }
     }
 
     private void CompleteDelay()
     {
         delayComplete = true;
-        Debug.Log("Camera delay complete!");
+        //Debug.Log("Camera delay complete!");
     }
 }
